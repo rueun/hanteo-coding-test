42131752a0ff25883793a82a7c825f42bc1f0d12
ejshin-muhayu <ejshin@muhayu.com>
2024-08-11 23:39:15 +0900

    feat: category 를 관리하는 CategoryManager 객체 생성

---
+++ src/main/java/org/example/question1/CategoryManager.java
@@ -0,0 +1,70 @@
+package org.example.question1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 카테고리 트리를 관리하는 역할
+ */
+class CategoryManager {
+    private final Map<Integer, Category> categoryMapById;
+    private final Map<String, Category> categoryMapByName;
+    private final List<Category> rootCategories;
+
+    public CategoryManager() {
+        this.categoryMapById = new HashMap<>();
+        this.categoryMapByName = new HashMap<>();
+        this.rootCategories = new ArrayList<>();
+    }
+
+    /**
+     * 새로운 카테고리를 추가합니다.
+     * @param categoryId 카테고리 식별자
+     * @param categoryName 카테고리 이름
+     * @param parentCategoryId 부모 카테고리 식별자 (null 이면 루트 카테고리임)
+     */
+    public void addCategory(final int categoryId, final String categoryName, final Integer parentCategoryId) {
+        final Category category = new Category(categoryId, categoryName, new ArrayList<>());
+        categoryMapById.put(categoryId, category);
+        categoryMapByName.put(categoryName, category);
+
+        // 부모 카테고리가 존재하면 자식 카테고리 추가
+        if (parentCategoryId == null) {
+            rootCategories.add(category);
+        } else {
+            categoryMapById.computeIfPresent(parentCategoryId, (k, parentCategory) -> {
+                final Category updatedParentCategory = parentCategory.addSubcategory(category);
+                categoryMapById.put(parentCategoryId, updatedParentCategory);
+                return updatedParentCategory;
+            });
+        }
+    }
+
+    /**
+     * 루트 카테고리들을 반환합니다.
+     * @return 루트 카테고리들의 리스트
+     */
+    public List<Category> getRootCategories() {
+        return new ArrayList<>(rootCategories);
+    }
+
+    /**
+     * 카테고리 ID로 특정 카테고리를 찾는다.
+     * @param categoryId 카테고리 ID
+     * @return 해당 ID의 카테고리
+     */
+    public Category findCategoryById(final int categoryId) {
+        return categoryMapById.get(categoryId);
+    }
+
+    /**
+     * 카테고리 이름으로 카테고리를 찾는다.
+     * @param categoryName 카테고리 이름
+     * @return 해당 이름의 카테고리
+     */
+    public Category findCategoryByName(final String categoryName) {
+        return categoryMapByName.get(categoryName);
+    }
+}
