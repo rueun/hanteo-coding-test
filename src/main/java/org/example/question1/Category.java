3e5f737dc2bd33f5015848f620bc3aa3c5ba3746
ejshin-muhayu <ejshin@muhayu.com>
2024-08-11 17:20:03 +0900

    feat: category 객체에 모든 카테고리 정보를 map 으로 변환하여 반환하는 메서드 추가

--- src/main/java/org/example/question1/Category.java
+++ src/main/java/org/example/question1/Category.java
@@ -1,7 +1,9 @@
 package org.example.question1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 카테고리 정보를 저장하는 불변 객체
@@ -29,5 +31,24 @@ record Category(long categoryId, String categoryName, List<Category> subcategori
         List<Category> updatedSubcategories = new ArrayList<>(subcategories);
         updatedSubcategories.add(subcategory);
         return new Category(categoryId, categoryName, updatedSubcategories);
+    }
+
+    /**
+     * 현재 Category 를 포함하여 모든 하위 카테고리까지 Map 형태로 변환하여 반환한다.
+     * @return 카테고리 정보를 담고 있는 Map
+     */
+    public Map<String, Object> toMapAllCategories() {
+        final Map<String, Object> result = new HashMap<>();
+        // 현재 카테고리 정보를 추가
+        result.put("id", categoryId);
+        result.put("name", categoryName);
+
+        // 하위 카테고리 목록을 변환
+        List<Map<String, Object>> subcategories = new ArrayList<>();
+        for (final Category subcategory : this.subcategories) {
+            subcategories.add(subcategory.toMapAllCategories());
+        }
+        result.put("subcategories", subcategories);
+        return result;
     }
 }
